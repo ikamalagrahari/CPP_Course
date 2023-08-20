edb0dd72fc831f45b38cb2dd1f8723787cc82c29
Kamal Agrahari <133527690+kamalagrahari03@users.noreply.github.com>
2023-08-20 20:03:24 +0530

    if Statements

---
+++ ifelse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m;
+    cout<<"m =";
+    cin>>m; 
+    if(m >40)
+    {
+        cout<<m<<"=PASS"<<endl;
+         }
+    else
+    {
+        cout<<m<<"=FAIL"<<endl;
+    }  
+    return 0;
+}
